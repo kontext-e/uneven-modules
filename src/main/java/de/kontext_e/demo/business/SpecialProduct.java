f55e32d6d0c48632dc31ef81b915c779acb19f4d
Gerd Aschemann <gerd@aschemann.net>
2017-08-01 20:16:12 +0200

    kontext-e/uneven-modules/#4 Create expected "WrongDirection" failure
    This works as expected: Introducing an import from business to facade
    is forbidden and leads to an architecture violation exception

---
+++ src/main/java/de/kontext_e/demo/business/SpecialProduct.java
@@ -0,0 +1,9 @@
+package de.kontext_e.demo.business;
+
+import de.kontext_e.demo.core.Core;
+import de.kontext_e.demo.facade.SpecialFacade;
+
+public class SpecialProduct {
+    private Core core;
+    private SpecialFacade specialFacade;
+}
