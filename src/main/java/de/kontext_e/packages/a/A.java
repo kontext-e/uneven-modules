7853a29729528042f267f9db910c6355ac69358b
Jens Nerche <j.nerche@kontext-e.de>
2019-02-15 14:54:41 +0100

    added example for simplest package diagram checking cleanup

---
+++ src/main/java/de/kontext_e/packages/a/A.java
@@ -0,0 +1,7 @@
+package de.kontext_e.packages.a;
+
+import de.kontext_e.packages.b.B;
+
+public class A {
+    private B b;
+}
---
+++ src/main/java/de/kontext_e/packages/b/B.java
@@ -0,0 +1,4 @@
+package de.kontext_e.packages.b;
+
+public class B {
+}
